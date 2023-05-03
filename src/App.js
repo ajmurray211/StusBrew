794550ecfeff7dd6ddadd22e72491fa13bc7f07a
AJ Murray <aj.murr4y@gmail.com>
2023-05-03 14:40:49 -0400

    commiting react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Stus brews site</h1>
+    </div>
+  );
+}
+
+export default App;
