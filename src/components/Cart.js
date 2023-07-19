020f06b0129e939887f7c84d291543a74542d152
AJ Murray <aj.murr4y@gmail.com>
2023-07-19 08:43:35 -0400

    implimented the new cart functionality

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,40 +1,37 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
-import { useCartFunctionality } from "../hooks/useCartFunctionality";
-import { ProductContext } from "../context/ProductContext";
+import { CartContext } from "../context/CartContext";
 
 const Cart = (props) => {
-    const { cart } = useContext(ProductContext)
-    const { loadCart, clearCart, toatalItemsInCart, runningCartTotal } = useCartFunctionality()
+    const { cart, clearCart, runningCartTotal, getCartItemCount } = useContext(CartContext);
 
-    const mappedCartItems = cart.map((item) => {
-        console.log(item)
-        return (
-            
-            <li>{item.name} - {item.count} - {item.price}</li>
-        )
-    })
+    const mappedCartItems = cart.map((item) => (
+        <li key={item.name}>
+            {item.name}, {item.price} x {item.qty} - ${item.price * item.qty}
+        </li>
+    ));
 
-    console.log(runningCartTotal, toatalItemsInCart())
     return (
         <div>
-            <Button id="cartButton" onClick={props.toggle} size="lg" color={runningCartTotal === 0 ? "light" : "warning"}>
-                <ShoppingCartIcon fontSize="large" />
-                {toatalItemsInCart()}
-            </Button>
-            <Modal centered size="lg" isOpen={props.isOpen} toggle={props.toggle} >
+            <div style={{ position: 'relative' }}>
+                <ShoppingCartIcon id="cartButton" onClick={props.toggle} />
+                {getCartItemCount() > 0 && (
+                    <div id="itemsInCart">{getCartItemCount()}</div>
+                )}
+            </div>
+            <Modal centered size="lg" isOpen={props.isOpen} >
                 <ModalHeader toggle={props.toggle}>Your Cart</ModalHeader>
                 <ModalBody>
-                    {runningCartTotal === 0 ? 'Your cart is empty' : mappedCartItems}
+                    {getCartItemCount() === 0 ? 'Your cart is empty' : mappedCartItems}
                 </ModalBody>
                 <ModalFooter >
                     <Button color="secondary" onClick={props.toggle}>
                         Check Out
                     </Button>
                     <Button color="secondary" onClick={() => {
-                        clearCart()
-                        props.toggle()
+                        clearCart();
+                        props.toggle();
                     }}>
                         Clear cart
                     </Button>
