56daa6a1e9f1278a7c14938609e49e8079f8b21f
AJ Murray <aj.murr4y@gmail.com>
2023-07-19 08:46:09 -0400

    edited the selecting of products and how fetched product displays, I am about to impliment fetching from an api

--- src/components/procucts/Details.js
+++ src/components/procucts/Details.js
@@ -1,70 +1,77 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
+import { useToggler } from '../../hooks/useToggler';
 import { ProductContext } from '../../context/ProductContext';
 import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Label } from 'reactstrap';
-import { useToggler } from '../../hooks/useToggler';
-import { useCartFunctionality } from '../../hooks/useCartFunctionality';
-import { redirect } from 'react-router';
+import { CartContext } from '../../context/CartContext';
 
 const Details = () => {
-    const { selectedProduct, cart, setCart } = useContext(ProductContext);
-    const [blend, setSetBlend] = useState('Yes');
-    const [selectedSize, setSelectedSize] = useState(selectedProduct ? selectedProduct.sizes[0] : { amount: 'error', price: 'null' })
-    const { saveCart, addItemToCart, removeItemFromCart, loadCart } = useCartFunctionality()
+    const { selectedProduct } = useContext(ProductContext);
+    const { addToCart } = useContext(CartContext)
+    const [blend, setBlend] = useState('Yes');
+    const [selectedSize, setSelectedSize] = useState(
+        selectedProduct ? selectedProduct.sizes[0] : { amount: 'error', price: 'null' }
+    );
+    const { isOpen: isOpenSizeDropdown, toggle: toggleSizeDropdown } = useToggler()
+    const { isOpen: isOpenBlendDropdown, toggle: toggleBlendDropdown } = useToggler()
 
-    const { isOpen: sizeDropdown, toggle: toggleSizeDropdown } = useToggler()
-    const { isOpen: blendDropdown, toggle: toggleBlendDropdown } = useToggler()
+    const handleSizeSelection = (size) => {
+        setSelectedSize(size);
+    };
 
-    const mappedSizes = selectedProduct.sizes.map((size) => {
-        return (
-            <DropdownItem onClick={() => setSelectedSize(size)}> {size.amount}</DropdownItem >
-        )
-    })
+    const handleBlendSelection = (selectedBlend) => {
+        setBlend(selectedBlend);
+    };
+
+    const handleAddToCart = () => {
+        addToCart({ productId: selectedProduct.productId, name: selectedProduct.name, price: selectedSize.price, qty: 1 });
+    };
+
+    const mappedSizes = selectedProduct.sizes.map((size) => (
+        <DropdownItem key={size.amount} onClick={() => handleSizeSelection(size)}>
+            {size.amount}
+        </DropdownItem>
+    ));
 
     return (
         <div className="detailsContainer">
             {selectedProduct.outOfStock && <div className="outOfStockBanner">Out of stock</div>}
-            <section className='prodPics'>
+            <section className="prodPics">
                 <img src={selectedProduct.src} alt={selectedProduct.name} />
             </section>
 
-            <section className='prodDetails'>
-                <section className='header'>
-                    <h1>{selectedProduct.name} </h1>
+            <section className="prodDetails">
+                <section className="header">
+                    <h1>{selectedProduct.name}</h1>
                     <p>${selectedSize.price}</p>
                 </section>
 
-                <section className='cartSeletion'>
-                    <div className='specs'>
+                <section className="cartSeletion">
+                    <div className="specs">
                         <section>
-                            <Label for='size'>Size:</Label>
-                            <Dropdown isOpen={sizeDropdown} toggle={toggleSizeDropdown} id='size'>
+                            <Label for="size">Size:</Label>
+                            <Dropdown isOpen={isOpenSizeDropdown} toggle={toggleSizeDropdown} id="size">
                                 <DropdownToggle caret>{selectedSize.amount}</DropdownToggle>
-                                <DropdownMenu>
-                                    {mappedSizes}
-                                </DropdownMenu>
+                                <DropdownMenu>{mappedSizes}</DropdownMenu>
                             </Dropdown>
                         </section>
                         <section>
-                            <Label for='blend'>Blend:</Label>
-                            <Dropdown isOpen={blendDropdown} toggle={toggleBlendDropdown} id='blend'>
+                            <Label for="blend">Blend:</Label>
+                            <Dropdown isOpen={isOpenBlendDropdown} toggle={toggleBlendDropdown} id="blend">
                                 <DropdownToggle caret>{blend}</DropdownToggle>
                                 <DropdownMenu>
-                                    <DropdownItem onClick={() => setSetBlend('Yes')}>Yes</DropdownItem>
-                                    <DropdownItem onClick={() => setSetBlend('No')}>No</DropdownItem>
+                                    <DropdownItem onClick={() => handleBlendSelection('Yes')}>Yes</DropdownItem>
+                                    <DropdownItem onClick={() => handleBlendSelection('No')}>No</DropdownItem>
                                 </DropdownMenu>
                             </Dropdown>
                         </section>
                     </div>
 
-                    <Button block color="dark" outline size="lg" onClick={() => {
-                        redirect('/home')
-                        addItemToCart(selectedProduct.name,selectedSize.price,1)
-                    }}>
+                    <Button block color="dark" outline size="lg" onClick={handleAddToCart}>
                         Add to cart
                     </Button>
 
-                    <div className='prodDescription'>
-                        <p> {selectedProduct.description}</p>
+                    <div className="prodDescription">
+                        <p>{selectedProduct.description}</p>
                         <p>Type: {selectedProduct.type}</p>
                         <p>Area: {selectedProduct.area}</p>
                     </div>
