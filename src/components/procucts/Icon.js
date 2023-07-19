b8db78d4bb414e60aa4376ec62d64f624f100854
AJ Murray <aj.murr4y@gmail.com>
2023-07-19 08:47:13 -0400

    updated functionality to use the new contexts as well as updatinging the classes and how dtaa presents

--- src/components/procucts/Icon.js
+++ src/components/procucts/Icon.js
@@ -1,28 +1,41 @@
 import { useContext, useState } from 'react';
 import { ProductContext } from '../../context/ProductContext';
+import { CartContext } from '../../context/CartContext';
 import { Link } from 'react-router-dom';
 
 const Icon = (props) => {
-    const { setSelectedProduct, selectedProduct } = useContext(ProductContext);
-    const [isHovered, setIsHovered] = useState(false);
+  const { setSelectedProduct } = useContext(ProductContext);
+  const { addToCart } = useContext(CartContext);
+  const [isHovered, setIsHovered] = useState(false);
 
-    const handleClick = () => {
-        setSelectedProduct(props.product);
-    };
+  const handleClick = () => {
+    setSelectedProduct(props.product);
+  };
 
-    return (
-        <div className="product"
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            onClick={handleClick}>
-            {props.product.outOfStock && <div className="outOfStockBanner">Out of stock</div>}
-            <Link className='iconDetails' to={`/details`}>
-                <img src={props.product.src[0]} alt={props.product.name} />
-                <p className={isHovered ? 'show' : ''}>{props.product.name} - {props.product.type} - {props.product.area}</p>
-                <p className={isHovered ? 'show' : ''}>Starting at: ${props.product.sizes[0].price}</p>
-            </Link>
-        </div >
-    );
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <Link
+      className='product'
+      to={`/details`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+    >
+      {props.product.outOfStock && <div className="outOfStockBanner">Out of stock</div>}
+      <img src={props.product.src[0]} alt={props.product.name} />
+      <div className={`productMeta ${isHovered ? 'show' : ''}`}>
+        <div className={isHovered ? 'show' : ''}>{props.product.name} - {props.product.type} - {props.product.area}</div>
+        <div id='price' className={isHovered ? 'show' : ''}>Starting at: ${props.product.sizes[0].price}</div>
+      </div>
+    </Link>
+  );
 };
 
 export default Icon;
