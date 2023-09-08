4ce40d776aaa4a6b59cf33f493fc863dd19c7fd3
AJ Murray <aj.murr4y@gmail.com>
2023-09-08 12:57:53 -0400

    added in checking okut funcitonality

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,13 +2,25 @@ import { useContext } from "react";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 import { CartContext } from "../context/CartContext";
+import axios from "axios";
 
 const Cart = (props) => {
-    const { cart, clearCart, runningCartTotal, getCartItemCount } = useContext(CartContext);
+    const { cart, clearCart, runningCartTotal, getCartItemCount, API_URL } = useContext(CartContext);
+
+    const handleCheckout = async () => {
+        axios.post(`${API_URL}create-checkout-session`, { cart, cart })
+            .then(res => {
+                console.log(res.ok)
+                window.location = res.data.url
+            })
+            .catch(e => {
+                console.error(e.error)
+            })
+    }
 
     const mappedCartItems = cart.map((item) => (
         <li key={item.name}>
-            {item.name}, {item.price} x {item.qty} - ${item.price * item.qty}
+            {item.name} - {item.size}, {item.priceInCents / 100} x {item.qty} - ${(item.priceInCents / 100) * item.qty}
         </li>
     ));
 
@@ -26,7 +38,7 @@ const Cart = (props) => {
                     {getCartItemCount() === 0 ? 'Your cart is empty' : mappedCartItems}
                 </ModalBody>
                 <ModalFooter >
-                    <Button color="secondary" onClick={props.toggle}>
+                    <Button color="secondary" onClick={handleCheckout}>
                         Check Out
                     </Button>
                     <Button color="secondary" onClick={() => {
