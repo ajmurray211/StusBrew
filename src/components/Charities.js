a34ec7d852f5fc6a8059d8e0b5596cd8dd899fd2
AJ Murray <aj.murr4y@gmail.com>
2023-09-08 12:53:10 -0400

    created generic components for charities and locations

---
+++ src/components/Charities.js
@@ -0,0 +1,34 @@
+const Charities = () => {
+    return (
+        <div className="charitiesContainer">
+            <div>
+                <ul>
+                    <li>
+                        <a href="https://stopsoldiersuicide.org/" target="_blank">
+                            Stop Soldier Suicide
+                        </a>
+                        : organization that supports active duty service members,
+                        veterans, and their families. They assist with mental health support and other
+                        services.
+                    </li>
+                    <li>
+                        <a href="https://www.shrinerschildrens.org/en" target="_blank">
+                            Shriners
+                        </a>
+                        : a health care organization that supports pediatric patients and their
+                        families regardless of their ability to pay or insurance status.
+                    </li>
+                    <li>
+                        <a href="https://buildamiracle.net/" target="_blank">
+                            Build a Miracle
+                        </a>
+                        : organization that works to build homes for families living in
+                        Mexico.
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Charities;
---
+++ src/components/Location.js
@@ -0,0 +1,12 @@
+const Location = () => {
+    return (
+        <div className="infoContainer">
+            <div>
+                See where Stus brews is going to be next. Currently we are mobile and mainly attend 
+                farmers markets.
+            </div>
+        </div>
+    );
+}
+
+export default Location;
