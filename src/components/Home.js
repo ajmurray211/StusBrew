08ff33fb80643a84399dd03d88afae6acb56e21c
AJ Murray <aj.murr4y@gmail.com>
2023-09-08 12:56:48 -0400

    added in locatins and charities components and started to use the context and hooks that were created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,44 @@
 import Scroller from './Scroller'
+import Location from './Location';
+import Charities from './Charities';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { Button } from 'reactstrap';
 import NewsLetter from './NewsLetter';
 import Icon from './procucts/Icon';
-import { beans, merch, mainImgs } from '../productData.js'
+import { mainImgs } from '../productData.js'
 import { Link } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import { CartContext } from '../context/CartContext';
+import { useDataFetcher } from '../hooks/useDataFetcher';
 
 const Home = () => {
+    const { API_URL } = useContext(CartContext)
+    const { getData: getMerch, data: merch, error: merchError, loading: merchLoading } = useDataFetcher();
+    const { getData: getBeans, data: beans, error: beanError, loading: beanLoading } = useDataFetcher();
+    useEffect(() => {
+        getMerch(`${API_URL}merch`)
+        getBeans(`${API_URL}bean`)
+    }, []);
+
     const mappedMerchItems = merch.map((product) => {
         return (
-            <Icon product={product} />
+            <Icon key={product._id} product={product} />
         )
     })
 
     const mappedBeanBags = beans.map((product) => {
         return (
-            <Icon product={product} />
+            <Icon key={product._id} product={product} />
         )
     })
 
     return (
         <div>
-            <Scroller
-                items={mainImgs} />
+            <Scroller items={mainImgs} />
+
+            <Location />
+
             <div className="prodBackground">
                 <h1>Coffee Beans</h1>
                 <div className="prodContainer">
@@ -31,8 +47,10 @@ const Home = () => {
                 <Button color='danger' size='lg'>
                     <Link className='link' to='/beanPage'>View all {<ArrowForwardIosIcon />}</Link>
                 </Button>
-                
+
             </div>
+
+            <Charities />
 
             <div className="prodBackground">
                 <h1>Stu's Styles</h1>
