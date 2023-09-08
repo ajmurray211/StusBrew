28d5ffee2b6de36bf67b4040b0ac49775b335798
AJ Murray <aj.murr4y@gmail.com>
2023-09-08 12:54:40 -0400

    added in context and hook functionality and mapped/ displayed data fetched from api

--- src/components/procucts/Details.js
+++ src/components/procucts/Details.js
@@ -1,21 +1,35 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useToggler } from '../../hooks/useToggler';
 import { ProductContext } from '../../context/ProductContext';
 import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Label } from 'reactstrap';
 import { CartContext } from '../../context/CartContext';
+import noImg from '../../Assets/noImageProvided.jpeg'
 
 const Details = () => {
     const { selectedProduct } = useContext(ProductContext);
-    const { addToCart } = useContext(CartContext)
+    const { addToCart, API_URL } = useContext(CartContext);
     const [blend, setBlend] = useState('Yes');
-    const [selectedSize, setSelectedSize] = useState(
-        selectedProduct ? selectedProduct.sizes[0] : { amount: 'error', price: 'null' }
-    );
-    const { isOpen: isOpenSizeDropdown, toggle: toggleSizeDropdown } = useToggler()
-    const { isOpen: isOpenBlendDropdown, toggle: toggleBlendDropdown } = useToggler()
+    const [prodId, setProdId] = useState(null)
+    const [selectedSize, setSelectedSize] = useState(selectedProduct && selectedProduct.variants && selectedProduct.variants.length > 0
+        ? selectedProduct.variants[0].sizeOptions[0]
+        : { name: 'error', priceInCents: 0 });
+    const { isOpen: isOpenSizeDropdown, toggle: toggleSizeDropdown } = useToggler();
+    const { isOpen: isOpenBlendDropdown, toggle: toggleBlendDropdown } = useToggler();
+
+    console.log(prodId)
+    useEffect(() => {
+        console.log(selectedProduct.variants)
+        if (selectedProduct.variants.length !== 0) {
+            setProdId(selectedProduct.variants[0].sizeOptions[0]._id)
+            setSelectedSize(selectedProduct && selectedProduct.variants && selectedProduct.variants.length > 0
+                ? selectedProduct.variants[0].sizeOptions[0]
+                : { name: 'error', priceInCents: 0 })
+        }
+    }, [])
 
     const handleSizeSelection = (size) => {
         setSelectedSize(size);
+        setProdId(size._id)
     };
 
     const handleBlendSelection = (selectedBlend) => {
@@ -23,61 +37,104 @@ const Details = () => {
     };
 
     const handleAddToCart = () => {
-        addToCart({ productId: selectedProduct.productId, name: selectedProduct.name, price: selectedSize.price, qty: 1 });
+        console.log(selectedSize)
+        addToCart({
+            productId: selectedSize._id,
+            name: selectedProduct.name,
+            priceInCents: selectedSize.priceInCents,
+            qty: 1,
+            size: selectedSize.name
+        });
     };
 
-    const mappedSizes = selectedProduct.sizes.map((size) => (
-        <DropdownItem key={size.amount} onClick={() => handleSizeSelection(size)}>
-            {size.amount}
-        </DropdownItem>
-    ));
+    const mappedSizes = selectedProduct?.variants?.[0]?.sizeOptions
+        ? selectedProduct.variants[0].sizeOptions.map((size) => (
+            <DropdownItem key={size._id} onClick={() => handleSizeSelection(size)}>
+                {size.name}
+            </DropdownItem>
+        ))
+        : null;
+
+    const mappedMiniPics = selectedProduct.variants[0] && selectedProduct.variants[0].productImages.length > 0
+        ? selectedProduct.variants[0].productImages.map((img, index) => (
+            <a href={`#${index}`} onClick={(e) => handleImageClick(e, index)}>
+                <img className='miniProdImg' src={`${API_URL}uploads/${img}`} alt={selectedProduct?.name} />
+            </a>
+        ))
+        : <img className='miniProdImg' src={noImg} />;
+
+    const mappedProdPics = selectedProduct.variants[0] && selectedProduct.variants[0].productImages.length > 0
+        ? selectedProduct.variants[0].productImages.map((img, index) => (
+            <img id={index} className='prodImg' src={`${API_URL}uploads/${img}`} alt={selectedProduct?.name} />
+        ))
+        : <img className='ProdImg' src={noImg} />;
+
+    function handleImageClick(e, index) {
+        e.preventDefault();
+        const targetImage = document.getElementById(index);
+        if (targetImage) {
+            targetImage.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
 
     return (
-        <div className="detailsContainer">
-            {selectedProduct.outOfStock && <div className="outOfStockBanner">Out of stock</div>}
-            <section className="prodPics">
-                <img src={selectedProduct.src} alt={selectedProduct.name} />
+        <div className='grid'>
+            <section className='miniProdImgs gridItem'>
+                {mappedMiniPics}
             </section>
+            <section className='prodImgs gridItem'>
+                {selectedProduct && selectedProduct.outOfStock && <div className="outOfStockBanner">Out of stock</div>}
+                {mappedProdPics}
+            </section>
+            <section className='detailsContainer gridItem'>
 
-            <section className="prodDetails">
-                <section className="header">
-                    <h1>{selectedProduct.name}</h1>
-                    <p>${selectedSize.price}</p>
-                </section>
+                <section className="prodDetails">
+                    <section className="header">
+                        <h1>{selectedProduct?.name}</h1>
+                        <p>${selectedSize.priceInCents / 100}</p>
+                    </section>
 
-                <section className="cartSeletion">
-                    <div className="specs">
-                        <section>
-                            <Label for="size">Size:</Label>
-                            <Dropdown isOpen={isOpenSizeDropdown} toggle={toggleSizeDropdown} id="size">
-                                <DropdownToggle caret>{selectedSize.amount}</DropdownToggle>
-                                <DropdownMenu>{mappedSizes}</DropdownMenu>
-                            </Dropdown>
-                        </section>
-                        <section>
-                            <Label for="blend">Blend:</Label>
-                            <Dropdown isOpen={isOpenBlendDropdown} toggle={toggleBlendDropdown} id="blend">
-                                <DropdownToggle caret>{blend}</DropdownToggle>
-                                <DropdownMenu>
-                                    <DropdownItem onClick={() => handleBlendSelection('Yes')}>Yes</DropdownItem>
-                                    <DropdownItem onClick={() => handleBlendSelection('No')}>No</DropdownItem>
-                                </DropdownMenu>
-                            </Dropdown>
-                        </section>
-                    </div>
+                    <section className="cartSeletion">
+                        <div className="specs">
+                            <section>
+                                <Label for="size">Size:</Label>
+                                <Dropdown isOpen={isOpenSizeDropdown} toggle={toggleSizeDropdown} id="size">
+                                    <DropdownToggle caret>{selectedSize?.name}</DropdownToggle>
+                                    <DropdownMenu>{mappedSizes}</DropdownMenu>
+                                </Dropdown>
+                            </section>
+                            {selectedProduct?.type && (
+                                <section>
+                                    <Label for="blend">Blend:</Label>
+                                    <Dropdown isOpen={isOpenBlendDropdown} toggle={toggleBlendDropdown} id="blend">
+                                        <DropdownToggle caret>{blend}</DropdownToggle>
+                                        <DropdownMenu>
+                                            <DropdownItem onClick={() => handleBlendSelection('Yes')}>Yes</DropdownItem>
+                                            <DropdownItem onClick={() => handleBlendSelection('No')}>No</DropdownItem>
+                                        </DropdownMenu>
+                                    </Dropdown>
+                                </section>
+                            )}
+                        </div>
 
-                    <Button block color="dark" outline size="lg" onClick={handleAddToCart}>
-                        Add to cart
-                    </Button>
+                        <Button block color="dark" outline size="lg" onClick={handleAddToCart}>
+                            Add to cart
+                        </Button>
 
-                    <div className="prodDescription">
-                        <p>{selectedProduct.description}</p>
-                        <p>Type: {selectedProduct.type}</p>
-                        <p>Area: {selectedProduct.area}</p>
-                    </div>
+                        <div className="prodDescription">
+                            <p>{selectedProduct?.description}</p>
+                            {selectedProduct?.type &&
+                                <>
+                                    <p>Type: {selectedProduct?.type}</p>
+                                    <p>Area: {selectedProduct?.region}</p>
+                                </>
+                            }
+                        </div>
+                    </section>
                 </section>
             </section>
         </div>
+
     );
 };
 
--- src/components/procucts/Icon.js
+++ src/components/procucts/Icon.js
@@ -2,14 +2,16 @@ import { useContext, useState } from 'react';
 import { ProductContext } from '../../context/ProductContext';
 import { CartContext } from '../../context/CartContext';
 import { Link } from 'react-router-dom';
+import noImg from '../../Assets/noImageProvided.jpeg'
 
 const Icon = (props) => {
-  const { setSelectedProduct } = useContext(ProductContext);
-  const { addToCart } = useContext(CartContext);
+  const { setSelectedProduct, selectedProduct } = useContext(ProductContext);
+  const { API_URL } = useContext(CartContext)
   const [isHovered, setIsHovered] = useState(false);
 
   const handleClick = () => {
     setSelectedProduct(props.product);
+    console.log(selectedProduct)
   };
 
   const handleMouseEnter = () => {
@@ -20,6 +22,12 @@ const Icon = (props) => {
     setIsHovered(false);
   };
 
+  // Perform checks before accessing properties
+  const variants = props.product.variants || [];
+  const sizeOptions = variants[0]?.sizeOptions || [];
+  const startingPrice = sizeOptions[0]?.priceInCents ? sizeOptions[0].priceInCents / 100 : 0;
+  const imgs = variants[0] ? variants[0].productImages : []
+
   return (
     <Link
       className='product'
@@ -29,10 +37,11 @@ const Icon = (props) => {
       onClick={handleClick}
     >
       {props.product.outOfStock && <div className="outOfStockBanner">Out of stock</div>}
-      <img src={props.product.src[0]} alt={props.product.name} />
+      <img src={imgs.length === 0 ? noImg : `${API_URL}uploads/${imgs[0]}`} alt={props.product.name} />
       <div className={`productMeta ${isHovered ? 'show' : ''}`}>
-        <div className={isHovered ? 'show' : ''}>{props.product.name} - {props.product.type} - {props.product.area}</div>
-        <div id='price' className={isHovered ? 'show' : ''}>Starting at: ${props.product.sizes[0].price}</div>
+        <div className={isHovered ? 'show' : ''}>{props.product.name}</div>
+        <br />
+        <div id='price' className={isHovered ? 'show' : ''}>Starting at: ${startingPrice}</div>
       </div>
     </Link>
   );
