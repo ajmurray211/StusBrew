9a5208772ef349ee8faca11d031ec2d82f1bdd13
AJ Murray <aj.murr4y@gmail.com>
2023-10-05 13:08:23 -0400

    created the products page and styled it

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,77 +1,55 @@
+import { useContext, useEffect, useState } from "react";
+import { useDataFetcher } from "../hooks/useDataFetcher";
+import { CartContext } from "../context/CartContext";
+import { Button } from "reactstrap";
+import Icon from "../components/Icon";
+
 const Products = () => {
-    return ( 
-        <div>Products page</div>
-     );
+    const { API_URL } = useContext(CartContext)
+    const [sectionSwitch, setSelectionSwitch] = useState('merch')
+    const toggle = (e) => { setSelectionSwitch(e.target.name) }
+    const { getData: getBean, data: bean, error: beanError, loading: beanLoading } = useDataFetcher();
+    const { getData: getMerch, data: merch, error: merchError, loading: merchLoading } = useDataFetcher();
+
+    useEffect(() => {
+        getMerch(`${API_URL}merch`)
+        getBean(`${API_URL}bean`)
+    }, []);
+
+    const mappedbeanItems = bean.map((product) => {
+        return (
+            <Icon product={product} />
+        )
+    })
+
+    const mappedMerchItems = merch.map((product) => {
+        return (
+            <Icon product={product} />
+        )
+    })
+
+    return (
+        <div id="productsPage">
+            <div id="productsWelcomeContainer">
+                <div id="productsAccentDiv" />
+                {/* <div className="title" id="productWelcomeMsg">
+                    <p>Merch</p>
+                    <p>&</p>
+                    <p>Beans</p>
+                </div> */}
+                <img id="welcomeImg" src="" />
+            </div>
+            <div id="productsSection">
+                <div id="productSelectorContainer">
+                    <Button name="merch" onClick={toggle}>Merch</Button>
+                    <Button name="beans" onClick={toggle}>Beans</Button>
+                </div>
+                <div id="productsContainer">
+                    {sectionSwitch == 'merch' ? mappedMerchItems : mappedbeanItems}
+                </div>
+            </div>
+        </div>
+    );
 }
- 
+
 export default Products;
-
-
-// import { Button } from "reactstrap";
-// import Icon from "./Icon";
-// import { beans } from '../productData.js'
-// import { useDataFetcher } from "../hooks/useDataFetcher";
-// import { useContext, useEffect } from "react";
-// import { CartContext } from '../context/CartContext';
-
-
-// const BeanPage = () => {
-
-//     const { API_URL } = useContext(CartContext)
-
-//     const { getData: getBean, data: bean, error: beanError, loading: beanLoading } = useDataFetcher();
-
-//     useEffect(() => {
-//         getBean(`${API_URL}bean`)
-//     }, []);
-
-//     const mappedbeanItems = bean.map((product) => {
-//         return (
-//             <Icon product={product} />
-//         )
-//     })
-
-//     return (
-//         <div id="beanPage" onClick={console.log('clicked bean')}>
-//             <h1>Coffee Beans</h1>
-//             <div id="totalBeanContainer">
-//                 {mappedbeanItems}
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default BeanPage;
-
-// import Icon from "./Icon";
-// import { useDataFetcher } from "../hooks/useDataFetcher";
-// import { useContext, useEffect } from "react";
-// import { CartContext } from '../context/CartContext';
-
-// const MerchPage = () => {
-//     const { API_URL } = useContext(CartContext)
-
-//     const { getData: getMerch, data: merch, error: merchError, loading: merchLoading } = useDataFetcher();
-
-//     useEffect(() => {
-//         getMerch(`${API_URL}merch`)
-//     }, []);
-
-//     console.log(merch)
-
-//     const mappedMerchItems = merch.map((product) => {
-//         return (
-//             <Icon product={product} />
-//         )
-//     })
-//     return (
-//         <div id="merchPage" onClick={console.log('clicked merch')}>
-//             <h1>Stu's Styles</h1>
-//             <div id="totalMerchContainer">
-//                 {mappedMerchItems}
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default MerchPage;
