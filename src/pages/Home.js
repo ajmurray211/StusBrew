cdce5704150d137015713893fe6a2b83a728b654
AJ Murray <aj.murr4y@gmail.com>
2023-10-07 11:31:49 -0400

    reclassed home components and changes the styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,13 +21,15 @@ const Home = () => {
         getBeans(`${API_URL}bean`)
     }, []);
 
-    const mappedMerchItems = merch.map((product) => {
+    const mappedMerchItems = merch.map((product, i) => {
+        if (i > 2) { return }
         return (
             <Icon key={product._id} product={product} />
         )
     })
 
-    const mappedBeanBags = beans.map((product) => {
+    const mappedBeanBags = beans.map((product, i) => {
+        if (i > 2) { return }
         return (
             <Icon key={product._id} product={product} />
         )
@@ -39,26 +41,25 @@ const Home = () => {
 
             <Location />
 
-            <div className="prodBackground">
-                <h1>Coffee Beans</h1>
-                <div className="productContainer">
+            <div className="homeCoffeeSection">
+                <h1 className='title'>Coffee Beans</h1>
+                <div className="homeIconContainer">
                     {mappedBeanBags}
                 </div>
                 <Button color='danger' size='lg'>
-                    <Link className='link' to='/beanPage'>View all {<ArrowForwardIosIcon />}</Link>
+                    <Link className='link ' to='/products'>View all {<ArrowForwardIosIcon />}</Link>
                 </Button>
-
             </div>
 
             <Charities />
 
-            <div className="prodBackground">
-                <h1>Stu's Styles</h1>
-                <div className="productContainer">
+            <div className="homeMerchSection">
+                <h1 className='title'>Stu's Styles</h1>
+                <div className="homeIconContainer">
                     {mappedMerchItems}
                 </div>
                 <Button color='danger' size='lg'>
-                    <Link className='link' to='/merchPage'>View all {<ArrowForwardIosIcon />}</Link>
+                    <Link className='link' to='/products'>View all {<ArrowForwardIosIcon />}</Link>
                 </Button>
             </div>
 
